13a1fd0b3d7649842628d6a9b924a2bda35f922a
Daniel Kwong <dkwon014@ucr.edu>
2019-08-31 13:58:00 -0700

    format pokemon app

--- POKEMON APP/app.js
+++ POKEMON APP/app.js
@@ -1,10 +1,10 @@
 var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-    var input = document.querySelector(".pokemon-input");
-    var pokemonName = document.querySelector(".pokemon-name");
-    var pokemonImage = document.querySelector(".pokemon-image");
+var input = document.querySelector(".pokemon-input");
+var pokemonName = document.querySelector(".pokemon-name");
+var pokemonImage = document.querySelector(".pokemon-image");
 
-    function getPokemonData() {
-        axios.get(apiUrl + input.value)
+function getPokemonData() {
+    axios.get(apiUrl + input.value)
         .then(function (response) {
             pokemonName.innerHTML = response.data.forms[0].name;
             pokemonImage.src = response.data.sprites.front_default;
@@ -13,7 +13,7 @@ var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
             pokemonName.innerHTML = "(An error has occurred.)";
             pokemonImage.src = "";
         });
-    }
+}
 
-    var button = document.querySelector(".pokemon-button");
-    button.addEventListener("click", getPokemonData);
+var button = document.querySelector(".pokemon-button");
+button.addEventListener("click", getPokemonData);
